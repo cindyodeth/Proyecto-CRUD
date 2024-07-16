5c6fbb1c6ab195001bbef06d110080447c148dc2
C1ndy2024 <cbentacur2024@gmail.com>
2024-07-16 16:32:09 -0600

    primer commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import express from 'express'
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), ()=>
+console.log('server listening on port', app.get('port')));
